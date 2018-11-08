4ecf8a296edf29bdde2803fa607d31d559169873
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-11-08 11:43:17 -0500

    Classe technique pour consommer un service de données web

---
+++ MoutonsRapides/src/donnee/ServiceWeb.java
@@ -0,0 +1,64 @@
+package accesseur;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringBufferInputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+public class ServiceWeb {
+
+	public static String lireBalise(Element element, String balise)
+	{
+		return element.getElementsByTagName(balise).item(0).getTextContent();
+	}
+	
+	public static Document parserXML(String xml)
+	{
+		Document doc = null;
+		DocumentBuilder parseur;
+		try {
+			parseur = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			doc = parseur.parse(new StringBufferInputStream(xml));		
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return doc;
+	}
+	
+	public static String consommerService(String url)
+	{
+		String xml = null;
+		try {
+			URL urlServiceMouton = new URL(url);
+			URLConnection serviceMouton = urlServiceMouton.openConnection();
+			InputStream fluxMouton = serviceMouton.getInputStream();
+			
+			Scanner lecteur = new Scanner(fluxMouton).useDelimiter("\\A");
+			xml = lecteur.hasNext() ? lecteur.next() : "";
+			System.out.println(xml);
+			return xml;
+			
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
